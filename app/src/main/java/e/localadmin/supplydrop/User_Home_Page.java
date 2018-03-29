f607533e5ccad6d097d16c49d774ef8202d392b2
Daniel Grimm <grimmd2@uw.edu>
2018-03-29 16:23:00 -0700

    Added survey activity.

---
+++ app/src/main/java/e/localadmin/supplydrop/User_Home_Page.java
@@ -0,0 +1,13 @@
+package e.localadmin.supplydrop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class User_Home_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user__home__page);
+    }
+}
