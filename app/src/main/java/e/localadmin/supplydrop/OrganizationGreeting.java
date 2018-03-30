99b0a5bf4a5db378fa4743e0e0c23a2b9d63df74
Daniel Grimm <grimmd2@uw.edu>
2018-03-29 17:14:44 -0700

    Adding frameworks for pages.

--- app/src/main/java/e/localadmin/supplydrop/OrganizationGreeting.java
+++ app/src/main/java/e/localadmin/supplydrop/OrganizationGreeting.java
@@ -3,11 +3,11 @@ package e.localadmin.supplydrop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Greeting extends AppCompatActivity {
+public class OrganizationGreeting extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_greeting);
+        setContentView(R.layout.activity_organization_greeting);
     }
 }
