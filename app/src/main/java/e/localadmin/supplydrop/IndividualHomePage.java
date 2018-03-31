62963d93384dffb4c6d3323c35df7dd60cb6ab0b
Daniel Grimm <grimmd2@uw.edu>
2018-03-30 22:34:25 -0700

    Adding new activity. Finishing up code skeletons.

--- app/src/main/java/e/localadmin/supplydrop/IndividualHomePage.java
+++ app/src/main/java/e/localadmin/supplydrop/IndividualHomePage.java
@@ -3,7 +3,7 @@ package e.localadmin.supplydrop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class User_Home_Page extends AppCompatActivity {
+public class IndividualHomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
