3c24e84c42d2395ccab26e683ea3b87686af1577
Daniel Grimm <grimmd2@uw.edu>
2018-03-30 22:54:24 -0700

    Deleting trash AWS stuff. It's too much to handle at the moment and keeps breaking everything.

--- app/src/main/java/e/localadmin/supplydrop/SignInPage.java
+++ app/src/main/java/e/localadmin/supplydrop/SignInPage.java
@@ -67,7 +67,7 @@ public class SignInPage extends AppCompatActivity implements LoaderCallbacks<Cur
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.username);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -82,7 +82,7 @@ public class SignInPage extends AppCompatActivity implements LoaderCallbacks<Cur
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
