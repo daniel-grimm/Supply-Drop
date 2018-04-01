131e74c82dda6631d2e8c8aa15b62428ed171824
Daniel Grimm <grimmd2@uw.edu>
2018-03-31 20:28:34 -0700

    Saving work. Trying to change the default activity to debug switching between activities.

--- app/src/main/java/e/localadmin/supplydrop/IndividualGreeting.java
+++ app/src/main/java/e/localadmin/supplydrop/IndividualGreeting.java
@@ -2,6 +2,9 @@ package e.localadmin.supplydrop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.content.Intent;
 
 public class IndividualGreeting extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class IndividualGreeting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
+    }
+
+    /**This method is called when the get started button is clicked.
+     * This activity exits and then loads up the request form.*/
+    public void goToRequestForm(View view) {
+        Intent requestFormPage = new Intent(this, RequestForm.class);
+        startActivity(requestFormPage);
     }
 }
