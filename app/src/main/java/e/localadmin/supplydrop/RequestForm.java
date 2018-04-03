ba57ae9ae29ce15cdc69419be89cd3b1f469c988
Daniel Grimm <grimmd2@uw.edu>
2018-04-02 18:45:10 -0700

    Saving work.

--- app/src/main/java/e/localadmin/supplydrop/RequestForm.java
+++ app/src/main/java/e/localadmin/supplydrop/RequestForm.java
@@ -2,6 +2,8 @@ package e.localadmin.supplydrop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class RequestForm extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class RequestForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__home__page);
+    }
+
+    public void submitRequest(View view) {
+        Intent newRequest = new Intent(RequestForm.this, RequestForm.class);
+
+
+
+
+        startActivity(newRequest);
+    }
+
+    private boolean validateAmountsRequested() {
+        return false;
     }
 }
---
+++ app/src/main/java/e/localadmin/supplydrop/SupplyRequestLimits.java
@@ -0,0 +1,22 @@
+/**This class determines the limits of supplies that can be ordered
+ * in a week timeframe.
+ * @author Daniel Grimm*/
+
+package e.localadmin.supplydrop;
+
+public final class SupplyRequestLimits {
+
+    //Food request limits
+    public final int FOOD_LIMIT = 3;
+
+    //Apparel request limits
+    public final int NUM_COATS = 1;
+    public final int NUM_SLEEPING_MATERIALS = 1;
+    public final int NUM_SOCKS = 2;
+    public final int NUM_UNDERWEAR = 2;
+
+    //Hygiene products request limits
+    public final int NUM_TOOTHBRUSH = 1;
+    public final int NUM_TOOTHPASTE = 2;
+    public final int NUM_SOAP = 2;
+}
