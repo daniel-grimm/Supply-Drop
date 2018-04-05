d427af55c38521686bdffaf7483a100aedfd38ed
Daniel Grimm <grimmd2@uw.edu>
2018-04-05 15:14:56 -0700

    Updating the dashboard

--- app/src/main/java/e/localadmin/supplydrop/TheDashboard.java
+++ app/src/main/java/e/localadmin/supplydrop/TheDashboard.java
@@ -11,9 +11,6 @@ import android.content.Intent;
 import android.view.View;
 
 //For the graph
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class TheDashboard extends AppCompatActivity {
 
@@ -22,15 +19,6 @@ public class TheDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_dashboard);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graph.addSeries(series);
     }
 
     /**Send the user to the map view.*/
