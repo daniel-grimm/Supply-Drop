a61b6202f3e0b7e1c51b5aa29f488b903a909998
Daniel Grimm <grimmd2@uw.edu>
2018-04-16 19:33:41 -0700

    Adding Firebase functionality to the app.

--- app/src/main/java/e/localadmin/supplydrop/SignInPage.java
+++ app/src/main/java/e/localadmin/supplydrop/SignInPage.java
@@ -17,7 +17,6 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 //Language imports
 import java.util.Arrays;
@@ -49,11 +48,6 @@ public class SignInPage extends Activity {
                         .setAvailableProviders(providers)
                         .build(),
                 RC_SIGN_IN);
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        DatabaseReference databaseReference = Database.DATABASE.getReference();
-        DatabaseReference users = databaseReference.child("Users");
-        DatabaseReference requests = databaseReference.child("Requests");
     }
 
     /*This sends the user to a greeting message.*/
