ec273f19688560a7bedc6dee2c3cc1f5db00554d
Daniel Grimm <grimmd2@uw.edu>
2018-04-26 16:39:33 -0700

    Adding request history to the app.

--- app/src/main/java/e/localadmin/supplydrop/OutstandingRequests.java
+++ app/src/main/java/e/localadmin/supplydrop/OutstandingRequests.java
@@ -10,13 +10,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 //Fire base imports
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class OutstandingRequests extends AppCompatActivity {
 
@@ -38,7 +42,30 @@ public class OutstandingRequests extends AppCompatActivity {
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                //
+                Iterable<DataSnapshot> ds = dataSnapshot.getChildren();
+
+                for (DataSnapshot dataSnap : ds) {
+                    HashMap<String, Object> wrapper = (HashMap<String, Object>) dataSnap.getValue();
+                    HashMap<String, Object> map = (HashMap<String, Object>) wrapper.get("map");
+                    String user = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+                    String storedUser = (String) map.get("user");
+                    if (!storedUser.equals(user)) {
+                        continue;//not the same user
+                    } else {
+                        //TODO:
+                    }
+                }
+
+                TextView food = findViewById(R.id.food_stuff);
+                TextView sleepingStuff = findViewById(R.id.sleeping_stuff);
+                TextView socks = findViewById(R.id.socks_stuff);
+                TextView underwear = findViewById(R.id.underwear_stuff);
+                TextView coat = findViewById(R.id.coat_stuff);
+                TextView toothbrush = findViewById(R.id.toothbrush_stuff);
+                TextView toothpaste = findViewById(R.id.toothpaste_stuff);
+                TextView soap = findViewById(R.id.soap_stuff);
+
+                food.setText("");
             }
 
             @Override
